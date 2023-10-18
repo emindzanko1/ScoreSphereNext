d664cf83546e1e317d8e847c5af64765f20af00b
emindzanko1 <edzanko1@etf.unsa.ba>
2023-10-18 14:37:56 +0200

    Added menu with leagues in search bar

--- components/layout/SearchBar.js
+++ components/layout/SearchBar.js
@@ -1,7 +1,8 @@
-import { Fragment, useState } from 'react';
+import { Fragment, useState, useEffect } from 'react';
 import classes from './SearchBar.module.css';
 
 const SearchBar = props => {
+  const [isLoading, setIsLoading] = useState(false);
   const [searchText, setSearchText] = useState('');
 
   const handleSearchInputChange = event => {
@@ -13,11 +14,64 @@ const SearchBar = props => {
     setSearchText('');
   };
 
-  console.log(searchText);
+  const [leagues, setLeagues] = useState([]);
+
+  useEffect(() => {
+    const fetchLeagues = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch('http://localhost:5000/leagues');
+        if (!response.ok) {
+          throw new Error('API request failed');
+        }
+        const data = await response.json();
+        setLeagues(data.competitions);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+      setIsLoading(false);
+    };
+    fetchLeagues();
+  }, []);
+
+  let filteredLeagues;
+
+  if (leagues) {
+    filteredLeagues = leagues.filter(
+      league =>
+        !(
+          league.area.name === 'Europe' ||
+          league.area.name === 'Brazil' ||
+          league.area.name === 'World' ||
+          league.name === 'Championship' ||
+          league.area.name === 'South America' ||
+          league.name === 'Eredivisie' ||
+          league.name === 'Ligue 1' ||
+          league.name === 'Primeira Liga'
+        )
+    );
+  }
+
+  if (filteredLeagues) {
+    console.log(filteredLeagues);
+  }
 
   return (
     <form className={classes.searchForm}>
       <input type='text' value={searchText} onChange={handleSearchInputChange} />
+      <div className={classes.suggestionsList}>
+        {filteredLeagues ? (
+          filteredLeagues.map(league => (
+            <div className={classes.list} key={league.id}>
+              <img src={league.emblem} alt={league.name} />
+              {league.name}
+            </div>
+          ))
+        ) : (
+          <div className={classes.noMatches}>No matches found.</div>
+        )}
+      </div>
       {searchText && (
         <button type='button' onClick={handleClearSearch}>
           X
@@ -27,29 +81,6 @@ const SearchBar = props => {
         Close
       </button>
     </form>
-    // <form className='search-form'>
-    //   <input type='text' placeholder='Search' value={searchText} onChange={handleSearchInputChange} ref={inputRef} />
-    //   <div className='suggestions-list'>
-    //     {filteredValues.length > 0 ? (
-    //       filteredValues.map((filteredValue, index) => (
-    //         <div className='list' onClick={() => handleSuggestedTeamClick(filteredValue)} key={index}>
-    //           <img src={getFlagForName(filteredValue)} alt={`${filteredValue} flag`} /> {filteredValue}
-    //         </div>
-    //       ))
-    //     ) : (
-    //       <div className='no-matches'>No matches found.</div>
-    //     )}
-    //   </div>
-    //   {searchText ? (
-    //     <button type='button' onClick={handleClearSearch}>
-    //       X
-    //     </button>
-    //   ) : (
-    //     <button type='button' onClick={props.onCloseSearch}>
-    //       Close
-    //     </button>
-    //   )}
-    // </form>
   );
 };
 
